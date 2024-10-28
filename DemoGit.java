5aedd7d02743a1b51ab6e38a20a2018f40ce4d2a
Deepak Mishra <deepakmishra93152@gmail.com>
2024-10-28 18:49:09 +0530

    my first git demo

---
+++ DemoGit.java
@@ -0,0 +1,9 @@
+ public class DemoGit
+{
+    public static void main(String[] args) {
+        
+        for (int index = 0; index <7; index++) {
+            System.out.println(index);
+        }
+    }
+}
