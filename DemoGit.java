c6cff3083464e5f465da633029a71df37b8dd5d1
deep misra <deepakmishra93152@gmail.com>
2024-11-09 13:38:29 +0530

    files added

--- DemoGit.java
+++
@@ -1,9 +0,0 @@
- public class DemoGit
-{
-    public static void main(String[] args) {
-        
-        for (int index = 0; index <7; index++) {
-            System.out.println(index);
-        }
-    }
-}
